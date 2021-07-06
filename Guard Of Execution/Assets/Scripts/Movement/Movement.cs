0add0fa60ea2c4e7fba1b6b565a3d2ec71e09aff
witchzell <pw.chiarioni@gmail.com>
2021-07-06 18:55:54 +1200

    animation flow started

--- Guard Of Execution/Assets/Scripts/Movement/Movement.cs
+++ Guard Of Execution/Assets/Scripts/Movement/Movement.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Public for ease of change in the inspector (unity)
-    public float moveSpeed = 5f; // Horizontal mov
-    public float jUp = 5f; // Vertical mov
-    public bool Grounded = false; // Is player on the floor
+    public CharacterController2D controller;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Public for ease of change in the inspector (unity)
+    public float runSpeed = 5f;
+    private float horizontalSpeed = 0f;
+
+    bool jump = false;
+
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
-        Jump();
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed; //Change players speed taking in consideration of variables
+        horizontalSpeed = Input.GetAxisRaw("Horizontal") * runSpeed; // Input from player
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        
+        animator.SetFloat("Speed", Mathf.Abs(horizontalSpeed)); // Animation for running
+
     }
 
-    void Jump() //Jumping code
+    void FixedUpdate() //character movement
     {
-        if (Input.GetButtonDown("Jump") && Grounded == true)
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jUp), ForceMode2D.Impulse);
-        }
+        controller.Move(horizontalSpeed * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
